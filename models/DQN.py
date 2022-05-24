9e8cf5e93f882318b0f016ed91d32f843ad30fe4
neverparadise <ye20013@gmail.com>
2022-05-24 12:58:13 +0900

    2022-05-24

--- models/DQN.py
+++ models/DQN.py
@@ -53,7 +53,8 @@ class ClassicCNN(nn.Module):
   
     def sample_action(self, obs_dict, eps):
       pixels = pixel_converter(obs_dict)        
-      time_delta = torch.tensor([obs_dict['timedelta']]).to(device)
+      time_delta = torch.tensor([obs_dict['timedelta']]).to(device,  dtype=torch.float)
+      time_delta = time_scaler(time_delta)
       time_delta = time_delta.unsqueeze(1)
       
       out = self.forward(pixels, time_delta)
@@ -62,6 +63,9 @@ class ClassicCNN(nn.Module):
           return random.randint(0, self.num_actions-1)
       else:
           return torch.argmax(out).item()
+
+def time_scaler(timedelta):
+    return timedelta * 1e-6
       
 def pixel_converter(observation):
     if len(observation['pixels'].shape) < 4:
@@ -70,7 +74,8 @@ def pixel_converter(observation):
         pixel_tensor = torch.tensor(pixel_tensor).to(device).float()
         pixel_tensor = pixel_tensor.unsqueeze(0)
     else:
-        pixel_tensor = pixel_tensor.transpose(2, 0, 1)
+        pixel_tensor = observation['pixels']
+        pixel_tensor = pixel_tensor.transpose(0, 3, 1, 2)
         pixel_tensor = torch.tensor(pixel_tensor).to(device).float()
     return pixel_tensor
 
@@ -88,13 +93,10 @@ def make_batch(memory, batch_size):
     
     # observations, next_observations, rewards, dones
     for ts, next_ts in zip(time_steps, next_timesteps):
-      #print('before pixel shape : {}'.format(ts.observation['pixels'].shape)) # (120, 80, 1)
       obs_pixel = pixel_converter(ts.observation)
-      #print('after pixel shape : {}'.format(obs_pixel.shape)) # (1, 1, 120, 80)
-      
-      obs_timedelta = ts.observation['timedelta']
+      obs_timedelta = time_scaler(ts.observation['timedelta'])
       next_obs_pixel = pixel_converter(next_ts.observation)
-      next_obs_timedelta = next_ts.observation['timedelta']
+      next_obs_timedelta = time_scaler(next_ts.observation['timedelta'])
       
       obs_dicts['pixels'].append(obs_pixel)
       next_obs_dicts['pixels'].append(next_obs_pixel)
@@ -112,18 +114,27 @@ def make_batch(memory, batch_size):
 def train_dqn(behavior_net, target_net, memory, optimizer, gamma, batch_size):
     obs_dicts, actions, next_obs_dicts, rewards, dones = make_batch(memory, batch_size)
     cur_pixels = torch.cat(obs_dicts['pixels'], dim=0).to(device, dtype=torch.float)
-    cur_timedeltas = torch.stack(obs_dicts['timedelta']).to(device, dtype=torch.long)
+    cur_timedeltas = torch.stack(obs_dicts['timedelta']).to(device, dtype=torch.float)
     actions = torch.stack(actions).to(device, dtype=torch.long)
     reward = torch.stack(rewards).to(device, dtype=torch.float)
     next_pixels = torch.cat(next_obs_dicts['pixels'], dim=0).to(device, dtype=torch.float)
-    next_timedeltas = torch.stack(next_obs_dicts['timedelta']).to(device, dtype=torch.long)
+    next_timedeltas = torch.stack(next_obs_dicts['timedelta']).to(device, dtype=torch.float)
     dones = torch.stack(dones).to(device, dtype=torch.float)
-    
+
+    # print(f'cur_pixels : {cur_pixels}')
+    print(f'cur_timedeltas : {cur_timedeltas}')
     q_out = behavior_net.forward(cur_pixels, cur_timedeltas)
+    # print(f'q_out : {q_out}')
+
     q_a = q_out.gather(1, actions)
     max_target_q = target_net(next_pixels, next_timedeltas).max(1)[0].unsqueeze(1).detach()
+    # print(f'max_target_q : {max_target_q}')
+
     target = reward + gamma * max_target_q * dones
-    loss = F.smooth_l1_loss(q_a, target).to(device)
+    print(f'target : {target}')
+    print(f'q_a : {q_a}')
+
+    loss = F.mse_loss(q_a, target).to(device)
 
     optimizer.zero_grad()
     loss.backward()
